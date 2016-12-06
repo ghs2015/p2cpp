98b019316a555b8c2707b6306cabaa67f7c05833
Ray <zhangx3372@outlook.com>
2016-12-05 21:41:28 -0500

    AST Familied built but not tested with the parser

--- ast.cpp
+++ ast.cpp
@@ -156,28 +156,69 @@ void treeFree(Ast *a) {
                 << a->getNodetype() << std::endl;;
   }
 }
-
-Ast* evalFunction(Ast* astf){
+//For calling a function
+Ast* evalCall(Ast* astCall){
 	//！！这个函数君的人生目标就是运行一遍函数中的所有语句：
 	//目前只包括赋值语句和打印语句。然后返回一个值。如果返回值为空，那就返回一个Ast空空屁。
 	//不然就返回一个AstReturn.就用这个颜色一会吧，我打的字比较清楚。
-	//$开始一个新的scope
-	stlptr->push_scope();
-	//$尸位素餐的代码；
-
+	//发现一个问题，怎样区分函数的定义和调用呢？def fun用新的node？call fun用callNode?
+	//所以，有一种新的“对象”时，就可以定义一个新的类，把和她相关的乱七八糟的东西放在一起。
+	//如果她和别的类有共同点，就可以用继承，多态来优化代码。加上上面的封装，就是OO三大特征了！
+	//这样也方便自顶向下的设计程序。实践增强理解啊！理论是实践的抽象，但并不是实践。
+	//开始一个新的scope
+	Ast* astReturn;
+	std::vector<Ast*> :: iterator it ;
+	std::vector<Ast*> vec;	
+	stlptr -> push_scope();
+	//根据函数名，查表得到要执行的函数的AstFunction,进而得到其suiteNode的vector，
+	//然后eval(asts in the vec)，还要记得搞到returnNode。
+	// stlptr -> lookup_symbol(astCall->getName()) is a AstFunction* 
+	//这里需要加一个查询语句。
+	if (!stlptr ->find_symbol(astCall->getName())){ throw std::string("The function is not defined!"); }
+	vec = stlptr -> lookup_symbol(astCall->getName()) ->get_suite()->getVec();	
+	//遍历astf->vec里的所有AST NODE，用eval()进行逐条执行。
+	it = vec.begin();
+	while(it!=vec.end()){
+		eval(*it);
+		++it;
+	}
+	//这时函数体已经运行完，returnNode应该也已经生成并且存在当前的符号表里了。取一下，谢谢！
+	//他的名字叫"return"! ！！注意，这个需要在parser里面完成。TODO@parser
+	astReturn = stlptr->lookup_symbol("return");
 	//我感觉还得加个赋值Node。AST家族不断发展壮大啊，威胁到地球和平了。。。
 	//好像还要用递归，哇嘎嘎，行不行啊，少废话了，快写！挂科了一会。。。
-	//$遍历astf->vec里的所有AST NODE，用eval()进行逐条执行。
-
-
-	//$关闭当前scope
-
+	
+	//关闭当前scope
+	stlptr->pop_scope();
+	return astReturn;
 }
+
+//for def a function
+//这哥们儿的目标是把一个函数的函数体suite存进它里面，那我还得有个evalSuite啊。。。
+Ast* evalFunction(Ast* ast){
+	//function 和suite好像不在一个scope里面啊。所以呢，先存function进去，然后，然后就没有然后了。。。
+	//那怎么存语句进这个functionNode呢？要在evalFunction之前，把astSuite做好，然后存在astFunction里就行了。
+	//这个要在建立AST的时候做，在AST建好后，astSuite也应该。。。应该什么。。。不对了。。。
+	//这样办吧，TODO@parser，在完成一个astSuite的生成的时候，把她存到astFunction里。
+	//！！TODO涉及了好多指针操作啊，没有memory leakage怎么好意思呢？？
+	//更新当前scope的符号表里的function node
+	stlptr->update_symbol(ast->getName(), ast);
+	return (new AstVoid('0', ast->getNodeid(), ast->getReturntype(), ast->getName()));
+}
+
+Ast* evalSuite(Ast* ast){
+	return (new AstVoid('0', ast->getNodeid(), ast->getReturntype(), ast->getName()));
+}
+
 //！！这家伙好像一个皮包公司啊。。。
 //！！怎样不用皮包+多条件判断？用polymophism？把返回类型等属性封装到类属性里，返回一个类的指针就行。
 //！！后面儿有时间再重构吧，代码会简洁直观不少。
 Ast* eval(Ast* ast){
-	//for function node
+	//for call function node
+	if(ast->getNodetype()=='C'){
+		return evalCall(ast);
+	}
+	//for def function node
 	if(ast->getNodetype()=='F'){
 		return evalFunction(ast);
 	}
@@ -195,16 +236,19 @@ Ast* eval(Ast* ast){
 		else { throw "What are you doing with the assign node?"; }
 		ast->getLeft()->setReturntype(rt);
 		ast->getLeft()->set_value(v);
-		stlptr->update_symbol(ast->getLeft()->getName(), ast->getLeft());		
+		stlptr->update_symbol(ast->getLeft()->getName(), ast->getLeft());
+		return (new AstVoid('0', ast->getNodeid(), ast->getReturntype(), ast->getName()));
 	}
 	//for print node
 	if(ast->getNodetype()=='P'){
-		//！！需要operator overload啊
+		//需要operator overload啊,已经加了，安。。。
 		std::cout<<eval(ast->get_toPrint())<<std::endl;
+		return (new AstVoid('0', ast->getNodeid(), ast->getReturntype(), ast->getName()));
 	}
 	//for void node
 	if(ast->getNodetype()=='0'){
-		//啥也不干可以不？
+		//啥也不干可以不？原物奉还！空空屁来，空空屁回。
+		return ast;
 	}
 	//for int return type, char nodetype, int id, char rt, int n)
 	if(ast->getReturntype()=='i'){
@@ -213,4 +257,27 @@ Ast* eval(Ast* ast){
 	if(ast->getReturntype()=='f'){
 		return new AstFloat('K', ast->getNodeid(), 'f', evalFloat(ast) );
 	}
+	else 
+		{return (new AstVoid('0', ast->getNodeid(), ast->getReturntype(), ast->getName()));}
+}
+
+//operator overload is coming
+std::ostream& operator <<(std::ostream& os, const Ast * ast)
+{
+	switch (ast->getNodetype()) {
+		case 'I': 
+			os << (ast->getIntNumber());
+			break;
+		case 'K':
+			os << (ast->getFloatNumber());
+			break;
+		//！！理论上来说，evaluate之后不会出现N型了吧？
+		case 'N':
+			os<< (ast->get_value());
+			break;
+		case '0':
+			break;
+		//！！看起来目前不用考虑打印其他类型的东东。
+	}
+	return os;
 }
--- ast.h
+++ ast.h
@@ -4,7 +4,7 @@
 //  Further adapted by Zhang Xiang
 #ifndef _AST_H_
 #define _AST_H_
-
+#include <iostream>
 #include <string>
 #include <vector>
 extern void yyerror(const char*);
@@ -12,169 +12,193 @@ extern void yyerror(const char*, const char);
 
 class Ast{
 
-	public:
-	  Ast(char n, int id, char rt) : nodetype(n), nodeid(id), returntype(rt) {}
-	  virtual ~Ast() {}
-	  char getNodetype() const { return nodetype; }
-	  int getNodeid() const { return nodeid;}
-	  char getReturntype() const { return returntype;}
-	  void setReturntype(char nrt) {returntype = nrt;}
+public:
+	Ast(char n, int id, char rt) : nodetype(n), nodeid(id), returntype(rt) {}
+	virtual ~Ast() {}
+	char getNodetype() const { return nodetype; }
+	int getNodeid() const { return nodeid;}
+	char getReturntype() const { return returntype;}
+	void setReturntype(char nrt) {returntype = nrt;}
 
-	  virtual Ast* getLeft() const { throw std::string("No Left"); }
-	  virtual Ast* getRight() const { throw std::string("No Right"); }
-	  virtual double getFloatNumber() const { throw std::string("No Float Number"); }
-	  virtual int getIntNumber() const { throw std::string("No Int Number"); }
-	  virtual std::string getName() const { throw std::string("No Name"); }
-	  virtual std::string getFunction() const { throw std::string("No Function"); }
+	virtual Ast* getLeft() const { throw std::string("No Left"); }
+	virtual Ast* getRight() const { throw std::string("No Right"); }
+	virtual double getFloatNumber() const { throw std::string("No Float Number"); }
+	virtual int getIntNumber() const { throw std::string("No Int Number"); }
+	virtual std::string getName() const { throw std::string("No Name"); }
+	virtual std::string getFunction() const { throw std::string("No Function"); }
 
-	  virtual void set_value(double v) {throw std::string("No setter");}
-	  virtual double get_value() const {throw std::string("No Value");}
-	  virtual std::vector<Ast*> getVec() const {throw std::string("No Vector"); }
-	  virtual void push_back(Ast* ast) {throw std::string("No push_back()"); }
-	  virtual void print_ast() { throw std::string("No print_ast()"); }
-	  virtual Ast* get_toPrint() { throw std::string("No toPrint"); }
-	private:
-	  char nodetype;
-	  int nodeid;
-	  char returntype;
+	virtual void set_value(double v) {throw std::string("No setter");}
+	virtual double get_value() const {throw std::string("No Value");}
+	virtual std::vector<Ast*> getVec() const {throw std::string("No Vector"); }
+	virtual void push_back(Ast* ast) {throw std::string("No push_back()"); }
+	virtual void print_ast() { throw std::string("No print_ast()"); }
+	virtual Ast* get_toPrint() { throw std::string("No toPrint"); }
+	virtual Ast* get_suite() const { throw std::string("No suite");  }
+	virtual void set_suite(Ast* ast) {throw std::string("No set_suite"); }
+
+	virtual Ast* get_return() const {throw std::string("No return");}
+	virtual void set_return(Ast* ast){throw std::string("No set_return");}
+private:
+	char nodetype;
+	int nodeid;
+	char returntype;
 };
 
 class AstNode : public Ast {
-	public:
-	  AstNode(char nodetype, int id, char rt, Ast* l, Ast* r) : Ast(nodetype, id, rt), left(l), right(r) {}
-	  virtual ~AstNode() {}
-	  virtual Ast* getLeft() const  { return left; }
-	  virtual Ast* getRight() const { return right; }
+public:
+	AstNode(char nodetype, int id, char rt, Ast* l, Ast* r) : Ast(nodetype, id, rt), left(l), right(r) {}
+	virtual ~AstNode() {}
+	virtual Ast* getLeft() const  { return left; }
+	virtual Ast* getRight() const { return right; }
 
-	private:
-	  Ast *left;
-	  Ast *right;
+private:
+	Ast *left;
+	Ast *right;
 };
 
 class AstFloat : public Ast {
-	public:
-	  AstFloat(char nodetype, int id, char rt, double n) : Ast(nodetype, id, rt), number(n) {} 
-	  virtual ~AstFloat() {}
-	  virtual double getFloatNumber() const { return number; }
-	private:
-	  double number;
+public:
+	AstFloat(char nodetype, int id, char rt, double n) : Ast(nodetype, id, rt), number(n) {} 
+	virtual ~AstFloat() {}
+	virtual double getFloatNumber() const { return number; }
+private:
+	double number;
 };
 
 class AstInteger : public Ast {
-	public:
-	  AstInteger(char nodetype, int id, char rt, int n) : Ast(nodetype, id, rt), number(n) {} 
-	  virtual ~AstInteger() {}
-	  virtual int getIntNumber() const { return number; }
-	private:
-	  int number;
+public:
+	AstInteger(char nodetype, int id, char rt, int n) : Ast(nodetype, id, rt), number(n) {} 
+	virtual ~AstInteger() {}
+	virtual int getIntNumber() const { return number; }
+private:
+	int number;
 };
 
 class AstName : public Ast {
-	public:
-	  AstName(char nodetype, int id, char rt, std::string n, double v) : Ast(nodetype,id,rt), name(n),value(v){}
-	  virtual ~AstName(){}
-	  virtual std::string getName() const { return name; }
-	  virtual void set_value(double v){value = v;}
-	  virtual double get_value() const {return value;}
-	private:
-	  std::string name;
-	  double value;
+public:
+	AstName(char nodetype, int id, char rt, std::string n, double v) : Ast(nodetype,id,rt), name(n),value(v){}
+	virtual ~AstName(){}
+	virtual std::string getName() const { return name; }
+	virtual void set_value(double v){value = v;}
+	virtual double get_value() const {return value;}
+private:
+	std::string name;
+	double value;
 };
-
+//!!Call node serves for updating the symbol table,nodetype='F'
 class AstFunction : public Ast {
-	public:
-	  AstFunction(char nodetype, int id, char rt, std::string n) : Ast(nodetype,id,rt), name(n){}
-	  virtual ~AstFunction(){}
-	  virtual std::string getName() const { return name; }
-	  virtual std::vector<Ast*> getVec() const {return vec; }
-	  virtual void push_back(Ast* ast) {vec.push_back(ast);}
-	private:
-	  std::string name;
-	  //是一个一个往里放呢？还是一起往里放？一个一个吧。
-	  std::vector<Ast*> vec; //!!把赋值语句和打印语句都放在一起，这样按顺序执行就不会有XX影响
+public:
+	AstFunction(char nodetype, int id, char rt, std::string n) : Ast(nodetype,id,rt), name(n){}
+	virtual ~AstFunction(){}
+	virtual std::string getName() const { return name; }
+	virtual Ast* get_suite() const { return astSuite; }
+	virtual void set_suite(Ast* ast) {astSuite=ast; }
+
+private:
+	std::string name;
+	  //是一个一个往里放呢？还是一起往里放？一个一个吧。现在直接直接整块放suiteNode了。
+	Ast* astSuite; //!!把赋值语句和打印语句都放在一起，这样按顺序执行就不会有XX影响
 };
 
-class AstPrint : public Ast {
-    public:
-	  AstPrint(char nodetype,int id,char rt,std::string n,Ast* ast) : Ast(nodetype,id,rt), name(n),toPrint(ast) {}
-	  virtual ~AstPrint(){}
-	  //!!这要对<<进行操作符重载啊。。。~0.5h
-	  //！！还是再eval()里面print吧
-	  //virtual void print_ast() { std::cout<<eval(toPrint)<<std:endl; }
-	private:
-	  std::string name;
-	  Ast* toPrint;
-};
-//!! To validate
-class AstAss: public Ast {
-    public:
-	  AstAss(char nodetype,int id,char rt,\
-	  	       std::string n, Ast* la, Ast* ra): \
-	                Ast(nodetype,id,rt), name(n),l(la),r(ra) {}
-	  virtual ~AstAss(){}
-	  virtual Ast* getLeft() const  { return l; }
-	  virtual Ast* getRight() const { return r; }
-	private:
-	  std::string name;
-	  Ast* l;
-	  Ast* r;
-};
-
-
-//!!To do
-class AstSuite : public Ast {
-	public:
-	  AstSuite(char nodetype, int id, char rt, std::string n) : Ast(nodetype,id,rt), name(n){}
-	  virtual ~AstSuite(){}
-	  virtual std::string getSuite() const { return name; }
-	private:
-	  std::string name;
-};
-//!!To do
-class AstReturn : public Ast {
-	public:
-	  AstReturn(char nodetype, int id, char rt, std::string n) : Ast(nodetype,id,rt), name(n){}
-	  virtual ~AstReturn(){}
-	  virtual std::string getReturn() const { return name; }
-	private:
-	  std::string name;
-};
-//!!To do
+//!!Call node serves for revieval, nodetype='C'
 class AstCall : public Ast {
-	public:
-	  AstCall(char nodetype, int id, char rt, std::string n) : Ast(nodetype,id,rt), name(n){}
-	  virtual ~AstCall(){}
-	  virtual std::string getCall() const { return name; }
-	private:
-	  std::string name;
+public:
+	AstCall(char nodetype, int id, char rt, std::string n) : Ast(nodetype,id,rt), name(n){}
+	virtual ~AstCall(){}
+	virtual std::string getName() const { return name; }	
+private:
+	std::string name;
 };
 
-//!!To do
+//AstPrint 
+class AstPrint : public Ast {
+public:
+	AstPrint(char nodetype,int id,char rt,std::string n,Ast* ast) : Ast(nodetype,id,rt), name(n),toPrint(ast) {}
+	virtual ~AstPrint(){}
+	virtual Ast* get_toPrint() {return toPrint;}
+  //!!这要对<<进行操作符重载啊。。。~0.5h
+  //！！目前还是eval()里面print吧
+  //virtual void print_ast() { std::cout<<eval(toPrint)<<std:endl; }
+private:
+	std::string name;
+	Ast* toPrint;
+};
+//!! AstAss To validate
+class AstAss: public Ast {
+public:
+	AstAss(char nodetype,int id,char rt,\
+		std::string n, Ast* la, Ast* ra): \
+	Ast(nodetype,id,rt), name(n),l(la),r(ra) {}
+	virtual ~AstAss(){}
+	virtual Ast* getLeft() const  { return l; }
+	virtual Ast* getRight() const { return r; }
+private:
+	std::string name;
+	Ast* l;
+	Ast* r;
+};
+
+//AstSuite To do
+	//store the stmts in a suite by a vector
+class AstSuite : public Ast {
+public:
+	AstSuite(char nodetype, int id, char rt, std::string n) : Ast(nodetype,id,rt), name(n){}
+	virtual ~AstSuite(){}
+	virtual std::string getName() const { return name; }
+	virtual std::vector<Ast*> get_vector() const { return vec;}
+	virtual void push_back(Ast* ast) {vec.push_back(ast);}
+	virtual Ast* get_return() const {return astReturn;}
+	virtual void set_return(Ast* ast){astReturn = ast;}
+private:
+	std::string name;
+	Ast* astReturn;
+	std::vector<Ast*> vec;
+};
+
+//AstReturn, Name is reserved as "return"
+class AstReturn : public Ast {
+public:
+	AstReturn(char nodetype, int id, char rt, std::string n, double v) : Ast(nodetype,id,rt), name(n),value(v){}
+	virtual ~AstReturn(){}
+	virtual std::string getName() const { return name; }
+	virtual void set_value(double v){value = v;}
+	virtual double get_value() const {return value;}
+private:
+	std::string name;
+	double value;
+};
+
+//AstVoid To do, name reserved as void, or nodetype as 'O'
 class AstVoid : public Ast {
-	public:
-	  AstVoid(char nodetype, int id, char rt, std::string n) : Ast(nodetype,id,rt), name(n){}
-	  virtual ~AstVoid(){}
-	  virtual std::string getVoid() const { return name; }
-	private:
-	  std::string name;
+public:
+	AstVoid(char nodetype, int id, char rt, std::string n) : Ast(nodetype,id,rt), name(n){}
+	virtual ~AstVoid(){}
+	virtual std::string getName() const { return name; }
+private:
+	std::string name;
+};
+
+//!!AstNone 怎么用啊？！
+class AstNone : public Ast{
+public:
+	AstNone(char nodetype, int id, char rt, std::string n) : Ast(nodetype,id,rt), name(n){}
+	virtual ~AstNone(){}
+	virtual std::string getName() const { return name; }
+private:
+	std::string name;
 };
 
 //!!这里可以统一用一个evalAST~1h
 double evalFloat(Ast*);
-int evalInt(Ast*);
-//！！要定义eval啊
-Ast* eval(Ast*);
-Ast* evalFunction(AstFunction*);//！！用Ast作为返回值，可以想存啥，就存啥！早咋不这么用呢？。。。
-void treeFree(Ast*); // delete and free an AST 
+int    evalInt(Ast*);
+//！！要定义eval啊... 知道了啊。。。
+Ast*   eval(Ast*);
+Ast*   evalFunction(AstFunction*);//！！用Ast作为返回值，可以想存啥，就存啥！早咋不这么用呢？。。。
+Ast*   evalSuite(Ast* ast);
+Ast*   evalCall(Ast* ast);
+Ast*   evalPrint(Ast* ast);
+void   treeFree(Ast*); // delete and free an AST 
 
-
-
-
+std::ostream& operator <<(std::ostream& os, const Ast * ast);
 
 #endif
-// functionNode,
-// suiteNode, suite 是什么神？
-// returnNode, 
-// NoneNode,
-// callNode
-// voidNode
